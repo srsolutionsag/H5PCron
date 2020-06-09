536cef046c9fa077ba6d68c2fc9f23e632d8e642
Florian Wyss <fw@studer-raimann.ch>
2020-06-09 12:37:22 +0200

    WIP

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -36,6 +36,7 @@ class ComposerStaticInit1ce8e39467b28a745351043e28c93faa
         'srag\\LibrariesNamespaceChanger\\PHP72Backport' => __DIR__ . '/..' . '/srag/librariesnamespacechanger/src/PHP72Backport.php',
         'srag\\LibrariesNamespaceChanger\\PHP7Backport' => __DIR__ . '/..' . '/srag/librariesnamespacechanger/src/PHP7Backport.php',
         'srag\\LibrariesNamespaceChanger\\RemovePHP72Backport' => __DIR__ . '/..' . '/srag/librariesnamespacechanger/src/RemovePHP72Backport.php',
+        'srag\\LibrariesNamespaceChanger\\UpdatePluginReadme' => __DIR__ . '/..' . '/srag/librariesnamespacechanger/src/UpdatePluginReadme.php',
     );
 
     public static function getInitializer(ClassLoader $loader)
